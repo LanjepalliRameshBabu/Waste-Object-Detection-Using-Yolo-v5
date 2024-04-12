5b3d381625f0f3c636c52332266529379864f7cd
LanjepalliRameshBabu <rameshbabu.lanjepallids@gmail.com>
2024-04-12 09:15:39 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'LanjepalliRameshBabu',
+    author_email= 'rameshbabu.lanjepallids@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
